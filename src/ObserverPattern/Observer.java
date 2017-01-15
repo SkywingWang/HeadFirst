92078b6ec3addf609460b0d51cd28778502f2e33
wangty9153@126.com <wangty9153@126.com>
2017-01-15 13:41:44 +0800

    Create project

--- src/ObserverPattern/Observer.java
+++
@@ -1,8 +0,0 @@
-package ObserverPattern;
-
-/**
- * Created by SkywingWang on 17/1/15.
- */
-public interface Observer {
-    public void update(float temp,float humidity,float pressure);
-}
--- src/StrategyPattern/QuackBehavior.java
+++
@@ -1,8 +0,0 @@
-package StrategyPattern;
-
-/**
- * Created by SkywingWang on 17/1/9.
- */
-public interface QuackBehavior {
-    public void quack();
-}
